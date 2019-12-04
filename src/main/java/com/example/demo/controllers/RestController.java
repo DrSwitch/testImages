b34eadf65200d2d70515f42e89f5234a7c3b3823
DrSwitch <switchyndra@gmail.com>
2019-12-04 21:02:00 +0800

    it's alive

--- src/main/java/com/example/demo/controllers/RestController.java
+++ src/main/java/com/example/demo/controllers/RestController.java
@@ -1,16 +1,22 @@
 package com.example.demo.controllers;
 
 import com.example.demo.entity.RequestForm;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
+    private static final int IMG_WIDTH = 100;
+    private static final int IMG_HEIGHT = 100;
 
     @PostMapping("setImages")
     public RequestForm setImages(@RequestParam("links") List<String> linkList) {
@@ -19,7 +25,7 @@ public class RestController {
             requestForm.setSuccess(true);
 
             for (String link : linkList) {
-                getFile(link);
+                saveFile(link);
             }
 
         } catch (Exception e) {
@@ -30,25 +36,32 @@ public class RestController {
         return requestForm;
     }
 
-    private File getFile(String link) throws IOException {
+    private String saveFile(String link) throws IOException {
 
-        String fileName = "loaded.jpg";
+        String fileName = "loaded-" + new Date() + ".jpg";
         URL url = new URL(link);
 
         InputStream in = new BufferedInputStream(url.openStream());
         FileOutputStream out = new FileOutputStream(fileName);
 
-        System.out.println("Download start!");
-
-        byte buffer[] = new byte[1024];
-        while(in.read(buffer) != -1) {
-            out.write(buffer);
+        try {
+            BufferedImage image = ImageIO.read(new ByteArrayInputStream(in.readAllBytes()));
+            ImageIO.write(resizeImage(image, image.getType()), "jpg", out);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        System.out.println("Download finish!");
 
         in.close();
         out.close();
 
-        return new File("");
+        return fileName;
     }
-}
+
+    private static BufferedImage resizeImage(BufferedImage originalImage, int type) {
+        BufferedImage resizedImage = new BufferedImage(IMG_WIDTH, IMG_HEIGHT, type);
+        Graphics2D g = resizedImage.createGraphics();
+        g.drawImage(originalImage, 0, 0, IMG_WIDTH, IMG_HEIGHT, null);
+        g.dispose();
+
+        return resizedImage;
+    }}
